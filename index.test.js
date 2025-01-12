704ab1e9eb2cfb0cd38ca327af079dbddcc961e9
Sandeep <Sandeep>
2025-01-12 19:42:01 +0530

    failed test

--- index.test.js
+++ index.test.js
@@ -2,7 +2,7 @@ const init = require('./index');
 
 describe('Init function', () => {
   it('should return the correct value', () => {
-    expect(init(1)).toBe(1);
+    expect(init(1)).toBe(2);
     expect(init(2)).toBe(2);
     expect(init('test')).toBe('test');
     expect(init(true)).toBe(true);
